286b4b4e00eec46de8391e077b1e648c5ea901f9
Gunnar Oledal <gunnar.oledal@gmail.com>
2022-10-04 18:56:41 +0200

    More

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -26,18 +26,23 @@
       <nav ref="mainMenu" v-if="mobileMenuIsActive" class="flex flex-col items-end">
         <ul
             class="leading-loose text-xl transition-all duration-300 lg:block hover:text-emerald-900/70 text-emerald-900 text-right">
-          <li v-for="item in menuItems" :class="{'decoration-cyan-500 font-bold': (item.url == currentUrl)}"
-              class="group block whitespace-nowrap hover:text-black underline decoration-4 decoration-transparent hover:decoration-cyan-500 transition-all duration-200">
-            <a
-               :href="item.url">
+          <li v-for="item in menuItems" :class="{'decoration-cyan-500 font-bold': (item?.uri == currentUrl)}"
+          class="block"
+              >
+            <a class="group whitespace-nowrap hover:text-black underline decoration-4 decoration-transparent hover:decoration-cyan-500 transition-all duration-200"
+              :class="{'mr-8': !(item?.childItems?.nodes?.length)}"
+               :href="item?.connectedNode?.node?.uri">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                    stroke="currentColor"
                    class="group-hover:text-white transition-all duration-700 text-transparent inline-block w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
               </svg>
-              {{item.name}}
+              {{item?.label}}
             </a>
-
+            <svg @click="expand = !expand" v-if="item?.childItems?.nodes?.length" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-2 w-6 h-6 inline-block">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+            <a class="block mr-8 text-lg " v-if="expand" v-for="child in item?.childItems?.nodes" :href="item?.connectedNode?.node?.uri + '#'">{{child?.label}}</a>
           </li>
         </ul>
       </nav>
@@ -49,15 +54,19 @@
 import { onMounted, ref } from 'vue';
 import { onClickOutside } from '@vueuse/core'
 import autoAnimate from '@formkit/auto-animate';
+import type { getMenuItems } from '@src/ts/queries';
 const mobileMenuIsActive = ref(false);
 const mainMenu = ref<HTMLElement>(null);
 const mobileNavbar = ref<HTMLElement>(null);
 
+const expand = ref<boolean>(false);
+
 defineProps<{
-  menuItems: {
-    name: string
-    url: string
-  },
+  // menuItems: {
+  //   name: string
+  //   url: string
+  // },
+  menuItems: Awaited<ReturnType<typeof getMenuItems>>,
   currentUrl: string
 }>()
 
