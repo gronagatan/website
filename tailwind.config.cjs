0d9edfec577bc70b2923e27664d7db4756231e73
Gunnar Oledal <gunnar.oledal@gmail.com>
2022-10-15 20:54:34 +0200

    making footer responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				patua	: ['"Patua One"', 'serif'],
-				// roboto: ['"Roboto Slab"', 'serif'],
-				// zilla: ['"Zilla Slab"', 'serif'],
-				sans: ["Jost", ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				test: "#456723"
-			}
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        patua: ['"Patua One"', 'serif'],
+        // roboto: ['"Roboto Slab"', 'serif'],
+        // zilla: ['"Zilla Slab"', 'serif'],
+        sans: ['Jost', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        test: '#456723'
+      },
+      gridTemplateColumns: {
+        'auto-fit-40': 'repeat(auto-fit, minmax(10rem, 1fr))'
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
 }
