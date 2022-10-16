16449d88329fc31ed0d202e4c3fc634990144657
Gunnar Oledal <gunnar.oledal@gmail.com>
2022-10-16 12:08:45 +0200

    NavBar is shaping up!

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,16 @@
 <template>
-  <div class="w-full p-6 flex items-center justify-between backdrop-blur-2xl shadow-xl">
-    <h2 class="font-patua text-fogra text-4xl">Brf Gröna Gatan</h2>
-    <nav ref="mainMenu" class="flex gap-12 text-xl">
-        <div v-for="item in menuItems" class="w-min" :class="{'decoration-cyan-500 font-bold': (item?.uri == currentUrl)}">
-          <a class="group whitespace-nowrap hover:text-black underline decoration-4 decoration-transparent hover:decoration-cyan-500 transition-all duration-200"
-             :href="item?.connectedNode?.node?.uri">
-            <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                 stroke="currentColor"
-                 class="group-hover:text-white transition-all duration-700 text-transparent inline-block w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-            </svg> -->
+  <div @mouseleave="expanded = false" ref="navBar" class="w-full p-6 flex justify-between text-kombugreen bg-baby-powder/40 backdrop-blur-2xl shadow-xl">
+    <h2 class="font-patua  text-4xl">Brf Gröna Gatan</h2>
+    <nav class="m-2 grid grid-flow-col grid-rows-1 auto-cols-fr gap-12 text-xl">
+        <div @mouseover="expanded = true" ref="menuRefs" v-for="item in menuItems" class="pb-2 border-b-2 border-dotted border-kombugreen flex flex-col" >
+          <a class="whitespace-nowrap hover:text-cyan-700 hover:underline"
+            :class="{'font-extrabold': (item?.connectedNode?.node?.uri == currentUrl)}"
+            :href="item?.connectedNode?.node?.uri + '#'">
             {{item?.label}}
           </a>
-          <!-- <svg v-if="item?.childItems?.nodes?.length" xmlns="http://www.w3.org/2000/svg"
-               fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-               class="ml-2 w-6 h-6 inline-block">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-          </svg> -->
-          <div class="w-0 overflow-visible">
-            <a class="" v-for="child in item?.childItems?.nodes"
-            :href="item?.connectedNode?.node?.uri + '#'">{{child?.label}}</a>
+          <div class="overflow-hidden transition-all duration-700" :class="{'!max-h-0': !expanded}">
+            <a class="block hover:text-cyan-700 hover:underline" v-for="child in item?.childItems?.nodes"
+            :href="(item?.connectedNode?.node?.uri + '#' + (child?.connectedNode?.node as Page).slug)">{{child?.label}}</a>
           </div>
         </div>
     </nav>
@@ -28,31 +19,26 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { onClickOutside } from '@vueuse/core'
-import autoAnimate from '@formkit/auto-animate';
 import type { getMenuItems } from '@src/ts/queries';
+import type { Page } from '@src/generated/graphql';
+
+const expanded = ref(false);
+const navBar = ref<HTMLElement>(null);
+const menuRefs = ref<HTMLElement[]>([]);
 
 defineProps<{
-  // menuItems: {
-  //   name: string
-  //   url: string
-  // },
   menuItems: Awaited<ReturnType<typeof getMenuItems>>,
   currentUrl: string
 }>()
 
-// function toggleMenu() {
-//   console.log('menu was toggled');
-//   mobileMenuIsActive.value = !mobileMenuIsActive.value;
-// }
-
-// onClickOutside(mobileNavbar, (ev) => {
-//   console.log('clicked outside menu');
-//   mobileMenuIsActive.value = false;
-// })
-
 onMounted(() => {
-  // autoAnimate(mobileNavbar.value);
+  menuRefs.value.forEach(ref => {
+    const foldoutDiv = ref.querySelector('div');
+    if(!foldoutDiv){
+      return;
+    }
+    foldoutDiv.style.maxHeight = foldoutDiv?.scrollHeight + 'px';
+  })
 })
 
 </script>
